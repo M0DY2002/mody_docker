e6a0ff8c34b98e52a22633bfb21bda1ed523417c
Mody <105606287+M0DY2002@users.noreply.github.com>
2024-03-07 17:17:45 +0100

    Update bash-preperation.sh
    mkdir -p

--- dashy/bash-preperation.sh
+++ dashy/bash-preperation.sh
@@ -1,10 +1,10 @@
-#create folder for docker
-mkdir $HOME/docker
 #create folder for dashy
-mkdir $HOME/docker/dashy
+mkdir -p $HOME/docker/dashy
 cd $HOME/docker/dashy
+
 #download initial configuration
 #https://github.com/Lissy93/dashy/blob/master/docs/configuring.md
+#local config.yml will be mapped to /app/public/item-icons
 wget https://raw.githubusercontent.com/Lissy93/dashy/master/public/conf.yml
 
 #later to-do for multi-page setup
